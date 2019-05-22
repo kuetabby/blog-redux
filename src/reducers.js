4253dc54ac3b7e12cf7bbef190ff9c6377e6d610
ivan <vansmuhammad@gmail.com>
2019-05-22 20:07:11 +0700

    Add reducers

---
+++ src/reducers.js
@@ -0,0 +1,30 @@
+import { FETCH_POST, FETCH_USER } from "../constants";
+
+const initial = {
+  data: []
+};
+export const postReducer = (state = initial, action) => {
+  switch (action.type) {
+    case FETCH_POST:
+      return {
+        ...state,
+        data: action.payload
+      };
+    default:
+      return state;
+  }
+};
+const initialUser = {
+  data: []
+};
+export const userReducer = (state = initialUser, action) => {
+  switch (action.type) {
+    case FETCH_USER:
+      return {
+        ...state,
+        data: [...state.data, action.payload]
+      };
+    default:
+      return state;
+  }
+};
